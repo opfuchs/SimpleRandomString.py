df79596527d599b942f8aadf1e3decdcd70b5c6d
cgiliberto <christian.giliberto@outlook.com>
2018-03-28 10:08:05 -0400

    Edited comments for readability, added authorship and license information

--- SimpleRandomString.py
+++ SimpleRandomString.py
@@ -3,15 +3,17 @@
 #was not intended for this purpose. SystemRandom() calls
 #CryptGenRandom() on Windows and /dev/urandom on *nix.
 # 
-# TODO: Update for python 3.6 using secrets module.
+# TODO: Incorporate secrets module
 # TODO: Make this a proper command-line application
 # without require user editing for string length. 
+#
+# (c) 2018 Christian Giliberto. MIT License.
 
 import random
 import string
 #import secrets TODO
 
-#Set N to desired string length
+#Set n to desired string length
 print(''.join(random.SystemRandom().choice(
     string.ascii_uppercase + string.digits + string.ascii_lowercase + string.punctuation) 
-    for _ in range(36)))
+    for _ in range(n)))
